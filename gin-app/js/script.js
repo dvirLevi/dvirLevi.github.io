d6fe7dbe38bd57c1b72de4f3f9af57b461665fcd
dvirLevi <dvirleviapp@gmail.com>
2019-04-29 22:48:57 +0300

    add img

--- gin-app/js/script.js
+++ gin-app/js/script.js
@@ -1,6 +1,7 @@
 const myPlants = document.getElementById('myPlants');
 const buttAddPlant = document.getElementById('buttAddPlant');
 
+
 let allId = 1;
 
 let arrPlants = [];
@@ -8,34 +9,41 @@ let arrPlants = [];
 
 
 const createPlants = {
-    // createBox() {
-    //     for (let x = 0; x < arrPlants.length; x++) {
-    //         let box = document.createElement('div');
-    //         box.className = 'boxPlant';
-    //         box.setAttribute('id', x);
-    //         if (arrPlants[x].img == "") {
-    //             let inp = document.createElement('input');
-    //             inp.setAttribute('type', 'file');
-    //             box.appendChild(inp);
-    //         } else {
-    //             let img = document.createElement('img');
-    //             img.src = arrPlants[x].img;
-    //             box.appendChild(img);
-    //         }
-    //         myPlants.appendChild(box)
-    //     }
-    // },
     addPlant() {
-        myPlants.innerHTML = "";
+
         arrPlants.push({
             img: "",
             id: allId
         });
         allId++;
-        // this.createBox();
+        this.renderElements();
+    },
+    renderElements() {
+        myPlants.innerHTML = "";
         for (let x = 0; x < arrPlants.length; x++) {
             box = new BoxPlant(arrPlants[x].img, arrPlants[x].id);
             box.createBox();
+        }
+    },
+    aploadFile(e) {
+        // var preview = document.querySelector('img');
+        console.log(e.target.parentElement.id)
+        let idForPushImg = e.target.parentElement.id;
+        let file = e.target.files[0];
+        let reader = new FileReader();
+
+        reader.addEventListener("load", function (e) {
+            console.log(reader.result)
+            for (let x in arrPlants) {
+                if (arrPlants[x].id == idForPushImg) {
+                    arrPlants[x].img = reader.result
+                }
+                createPlants.renderElements();
+            }
+        }, false);
+
+        if (file) {
+            reader.readAsDataURL(file);
         }
     }
 }
@@ -43,8 +51,9 @@ buttAddPlant.addEventListener('click', function () {
     createPlants.addPlant()
 });
 
+
 class BoxPlant {
-    constructor (img , id) {
+    constructor(img, id) {
         this.img = img;
         this.id = id;
     }
@@ -56,19 +65,22 @@ class BoxPlant {
         let buttAddImg = document.createElement('input');
         buttAddImg.className = 'butt-add-img';
         buttAddImg.setAttribute('type', 'file');
-        buttAddImg.setAttribute('id', 'buttAddImg');
+        buttAddImg.setAttribute('id', 'buttAddImg' + this.id);
+        buttAddImg.addEventListener("change", function (e) {
+            createPlants.aploadFile(e)
+        });
         box.appendChild(buttAddImg);
 
         let label = document.createElement('label');
         label.className = 'custom-file-upload';
-        label.setAttribute('for', 'buttAddImg');
+        label.setAttribute('for', 'buttAddImg' + this.id);
         // label.textContent = "הוספת תמונה";
         label.innerHTML += '<p class="w-75 m-0">הוספת תמונה</p><i class="fas fa-cloud-upload-alt w-25"></i>'
         box.appendChild(label);
 
-        // let img = document.createElement('img');
-        // img.src = this.img;
-        // box.appendChild(img);
+        let img = document.createElement('img');
+        img.src = this.img;
+        box.appendChild(img);
 
         myPlants.appendChild(box)
     }
